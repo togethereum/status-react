3152c09de7a633d2dab9525bad451115821942e3
Michele Balistreri <michele@bitgamma.com>
2022-01-26 14:57:42 +0300

    bump status-go

--- ios/StatusIm/AppDelegate.m
+++ ios/StatusIm/AppDelegate.m
@@ -194,13 +194,13 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
 
 - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler {
   if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
-    //TODO: currently refuses ALL authentications, it should instead accept all system certs + our trusted cert
+    //TODO: it should accept all system certs + our trusted cert
     NSURLSessionAuthChallengeDisposition disposition = NSURLSessionAuthChallengeCancelAuthenticationChallenge;
 
-    //__block NSURLCredential *credential = nil;
+    __block NSURLCredential *credential = nil;
 
-    //credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
-    //disposition = NSURLSessionAuthChallengeUseCredential;
+    credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+    disposition = NSURLSessionAuthChallengeUseCredential;
 
     if (completionHandler) {
       completionHandler(disposition, nil);
